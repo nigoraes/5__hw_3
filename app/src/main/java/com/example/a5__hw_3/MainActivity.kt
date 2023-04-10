2d0b2fd75fce99a7b2199daf588277af1e431fea
Nigora <esenbekovanigora@gmail.com>
2023-04-10 15:49:31 +0600

    исправила, удалила нечаяно

--- app/src/main/java/com/example/a5__hw_3/MainActivity.kt
+++ app/src/main/java/com/example/a5__hw_3/MainActivity.kt
@@ -2,10 +2,57 @@ package com.example.a5__hw_3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.a5__hw_3.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    var page = 1
+    var keySearch = ""
+    var adapter = ImageAdapter(arrayListOf())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initCLickers()
+    }
+
+    private fun initCLickers() {
+        binding.btnSearch.setOnClickListener {
+            page = 1
+            keySearch = binding.etSearch.text.toString()
+            adapter.cleanList()
+            requestImage()
+        }
+        binding.btnNext.setOnClickListener {
+            if (keySearch != binding.etSearch.text.toString()) {
+                adapter.cleanList()
+                page = 1
+                requestImage()
+            } else {
+                ++page
+                requestImage()
+            }
+        }
+
+    }
+
+    private fun requestImage() {
+        PixaService().api.getImage(binding.etSearch.text.toString(), page = page)
+            .enqueue(object : Callback<PixaModel> {
+                override fun onResponse(call: Call<PixaModel>, response: Response<PixaModel>) {
+                    if (response.isSuccessful) {
+                        adapter.addImage(response.body()!!.hits)
+                        binding.imageRecycler.adapter = adapter
+                    }
+                }
+
+                override fun onFailure(call: Call<PixaModel>, t: Throwable) {
+                    Log.e("aaa", "onFailure: ${t.message}")
+                }
+            })
     }
 }
